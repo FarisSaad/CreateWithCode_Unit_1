e11e83b0a1b3088fcd8a0674d236971f1a84c6d4
Faris AlShehri <faris@shehri.me>
2020-08-20 16:53:52 +0300

    Added Prototype 1 and Challenge 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float turnSpeed = 50.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    void OnMove (InputValue playerInput)
+    {
+
+        // reading player's
+        Vector2 playerMovement = playerInput.Get<Vector2>();
+        horizontalInput = playerMovement.x;
+        verticalInput = playerMovement.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move car forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        // Rotate car right/left
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
